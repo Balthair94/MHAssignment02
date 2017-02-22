e11d19ba166c2c2dc5abb655cbeecc4feed55a7c
balthair94 <archerbaltazar@gmail.com>
2017-02-21 23:55:13 -0600

    Js added to the skills area

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+function moveTo(id){
+	element = document.getElementById(id)
+	element.scrollIntoView(true);
+}
+
+
+//CODE FROM THE LIBRARY
+$('.avatar-container').hover(function(){
+  
+  if ($(this).find('.info').length <= 0) {
+    $(this).append('<div class="info"><div class="info-inner"></div></div>');
+  }
+  
+  var $info = $(this).find('.info'),
+      $inner= $(this).find('.info-inner'),
+      $p    = $(this).attr("class").match(/p-\w+/).toString().substring(2);
+  
+  $inner.text($p+'%');
+  $info.toggleClass('js-active');
+});
